c5ce33e90dc05aec9abec8ce9c2ce01254c30b7b
Anuj Gupta <89295349+AnujGupta4999@users.noreply.github.com>
2024-01-21 00:42:52 +0530

    Create NextGreaterElement.java

---
+++ Stack and Queue/NextGreaterElement.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+         if (nums2.length == 0 || nums1.length == 0)
+      return new int[0];
+
+    Map<Integer, Integer> map = new HashMap<>();
+    Stack<Integer> stack = new Stack<>();
+
+    stack.push(nums2[nums2.length - 1]);
+    map.put(nums2[nums2.length - 1], -1);
+
+    for (int i = nums2.length - 2; i >= 0; i--) {
+
+      if (nums2[i] < stack.peek()) {
+        map.put(nums2[i], stack.peek());
+        stack.push(nums2[i]);
+        continue;
+      }
+
+      while (!stack.isEmpty() && stack.peek() < nums2[i])
+        stack.pop();
+
+      if (stack.isEmpty()) {
+        stack.push(nums2[i]);
+        map.put(nums2[i], -1);
+      } else {
+        map.put(nums2[i], stack.peek());
+        stack.push(nums2[i]);
+      }
+    }
+
+    for (int i = 0; i < nums1.length; i++)
+      nums1[i] = map.get(nums1[i]);
+
+    return nums1;
+    }
+}
